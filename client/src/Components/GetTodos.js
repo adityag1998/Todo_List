bc6ebf97cd332a8aba28389dd9a0fb6b5d8b9a76
Aditya Gupta <adityag1998@gmail.com>
2021-03-01 00:50:09 +0530

    Fixed unique key rendering React
    1. React dev error removed
    2. Added relevant gitignore and remove unecessary dev modules from commit

--- client/src/Components/GetTodos.js
+++ client/src/Components/GetTodos.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { GET_TODOS } from '../GraphQL/Queries';
 
 function GetTodos() {
-  const { loading, error, data } = useQuery(GET_TODOS);
+  const { data } = useQuery(GET_TODOS);
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
-    setTodoList(data.todos);
+    if (data) {
+      setTodoList(data.todos);
+    }
   }, [data]);
+
   return (
     <>
-      {todoList.map((todo) => {
-        return <p>{todo.text}</p>;
-      })}
+      {todoList.map((todo) => (
+        <div key={`${todo.id}-todo-item`}>{todo.text}</div>
+      ))}
     </>
   );
 }
